eb99f055b0796140dadb9a2b43f7393900c30c1c
Bryce <muellerbryce8@me.com>
2016-03-11 14:42:10 -0500

    Removed all php code, sticky footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,10 @@
-//$("#contactForm").validator().on("submit", function (event) {
-//    if (event.isDefaultPrevented()) {
-//        formError();
-//        alert("Didn't work");
-//    } else {
-//        // everything looks good!
-//        event.preventDefault();
-//        submitForm();
-//        alert("JS worked");
-//    }
-//});
 
 $("#contactForm").submit(function(event){
     // cancels the form submission
     event.preventDefault();
-    submitForm();
+    formSuccess();
 });
 
-function submitForm(){
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: "php/process.php",
-        data: "&name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            }
-        }
-    });
-}
 function formSuccess(){
     $( "#msgSubmit" ).removeClass( "hidden" );
-}
-
-function formError(){
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Message Failed!")
 }
