80b66b9430c29cd94bb167bbd4cf189fcee9dc7e
Bryce <muellerbryce8@me.com>
2016-03-10 20:31:14 -0500

    Fixed typo in contact form and php site directory

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function submitForm(){
 
     $.ajax({
         type: "POST",
-        url: "php/process.php",
+        url: "brycemueller.github.com/php/process.php",
         data: "name=" + name + "&email=" + email + "&message=" + message,
         success : function(text){
             if (text == "success"){
